a96a2757d4c682136e97c5b7f5ae61b9d105bcf1
vishnu93tr <vishnu26121993@gmail.com>
2021-02-19 12:35:36 +0530

    implemented data provider and working solution in docker

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,15 +1,22 @@
 package tests;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -37,5 +44,21 @@ public class BaseTest {
     public void quitBrowser(){
         System.out.println("closing browser");
         driver.quit();
+    }
+    public static Object[][] getCases(String filename){
+        List<Object[]> list=new ArrayList<>();
+        try {
+            List lines=IOUtils.readLines(BaseTest.class.getClassLoader().getResourceAsStream(filename),"UTF-8");
+            JSONObject jsonObject=new JSONObject(lines.stream().collect(Collectors.joining()).toString());
+            JSONArray jsonArray=jsonObject.getJSONArray("cases");
+            for(Object o:jsonArray){
+                JSONObject object=(JSONObject)o;
+                list.add(new Object[]{object.getString("name"),object.getJSONObject("case").toString()});
+            }
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        return list.toArray(new Object[][]{});
     }
 }
